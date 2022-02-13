40f7fe49f038cd355f56e58941c4e9a2ffebecc9
付雷刚(10039464) <Flg@19891014>
2022-02-13 11:54:12 +0800

    修改在线环境配置

--- xcEdu_service/xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
+++ xcEdu_service/xc-govern-center/src/main/java/com/xuecheng/govern/center/GovernCenterApplication.java
@@ -2,9 +2,14 @@ package com.xuecheng.govern.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+/**
+ *
+ * @author fulg
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GovernCenterApplication {
     public static void main(String[] args) {
